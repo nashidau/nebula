4d81b81db371538a984306fca03c14300540d976
Brett Nash <nash@nash.id.au>
2010-08-31 16:03:24 +0800

    Add window

--- etacarinae/etac.c
+++ etacarinae/etac.c
@@ -28,9 +28,49 @@ elm_main(int argc, char **argv){
 		return 1;
 	}
 
+	window_create();
 
+	elm_run();
+	elm_shutdown();
 
 	return 0;
 
 }
 ELM_MAIN()
+
+static void
+win_main_del(void *data, Evas_Object *obj, void *event_info) {
+        elm_exit();
+}
+
+int
+window_create(void){
+	Evas_Object *win, *bg, *bx, *gl;
+
+	win = elm_win_add(NULL,"Eta Carinae", ELM_WIN_BASIC);
+	elm_win_title_set(win, "Eta Carinae");
+	evas_object_smart_callback_add(win, "delete,request", win_main_del, NULL);
+
+	bg = elm_bg_add(win);
+	evas_object_size_hint_weight_set(bg, 1.0, 1.0);
+        elm_win_resize_object_add(win, bg);
+        evas_object_show(bg);
+
+        bx = elm_box_add(win);
+        evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND,EVAS_HINT_EXPAND);
+        elm_win_resize_object_add(win, bx);
+        evas_object_show(bx);
+
+	gl = elm_genlist_add(win);
+        elm_genlist_always_select_mode_set(gl, false);
+        evas_object_size_hint_align_set(gl, EVAS_HINT_FILL, EVAS_HINT_FILL);
+        evas_object_size_hint_weight_set(gl, EVAS_HINT_EXPAND,EVAS_HINT_EXPAND);
+	evas_object_show(gl);
+	elm_box_pack_start(bx, gl);
+
+
+	evas_object_size_hint_min_set(bg, 480, 640);
+	evas_object_show(win);
+
+	return 0;
+}
