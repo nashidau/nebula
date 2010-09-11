a17ac3997aee8df80bb704428f05febd6757c1e8
Brett Nash <nash@nash.id.au>
2010-09-12 00:24:29 +0900

    Add groups and attributes

--- etacarinae/etac_chr.c
+++ etacarinae/etac_chr.c
@@ -8,19 +8,9 @@
 
 #define DEBUG(...) printf(__VA_ARGS__)
 
-static char *attr_label_get(const void *, Evas_Object *, const char *part);
-static Evas_Object *attr_icon_get(const void *, Evas_Object *, const char *part);
-static Eina_Bool attr_state_get(const void *, Evas_Object *, const char *part);
-static void attr_del(const void *, Evas_Object *);
-
-static const Elm_Genlist_Item_Class attr_class = {
-	.item_style = "default",
-	.func = {
-		.label_get = attr_label_get,
-		.icon_get = attr_icon_get,
-		.state_get = attr_state_get,
-		.del = attr_del,
-	}
+struct etac_attr_group {
+	const char *group;
+	Elm_Genlist_Item *item;
 };
 
 
@@ -49,6 +39,44 @@ enum {
 
 
 
+static char *attr_label_get(const void *, Evas_Object *, const char *part);
+static Evas_Object *attr_icon_get(const void *, Evas_Object *, const char *part);
+static Eina_Bool attr_state_get(const void *, Evas_Object *, const char *part);
+static void attr_del(const void *, Evas_Object *);
+
+static char *group_label_get(const void *, Evas_Object *, const char *part);
+static Evas_Object *group_icon_get(const void *, Evas_Object *, const char *part);
+static Eina_Bool group_state_get(const void *, Evas_Object *, const char *part);
+static void group_del(const void *, Evas_Object *);
+
+
+
+
+static const Elm_Genlist_Item_Class attr_class = {
+	.item_style = "default",
+	.func = {
+		.label_get = attr_label_get,
+		.icon_get = attr_icon_get,
+		.state_get = attr_state_get,
+		.del = attr_del,
+	}
+};
+
+static const Elm_Genlist_Item_Class group_class = {
+	.item_style = "default",
+	.func = {
+		.label_get = group_label_get,
+		.icon_get = group_icon_get,
+		.state_get = group_state_get,
+		.del = group_del,
+	}
+};
+
+Eina_List *groups;
+
+/**
+ * Attr callbacks for the genlist
+ */
 static char *
 attr_label_get(const void *eacv, Evas_Object *obj, const char *part){
 	struct etac_attr *eac = (void *)eacv;
@@ -65,20 +93,36 @@ attr_label_get(const void *eacv, Evas_Object *obj, const char *part){
 static Evas_Object *
 attr_icon_get(const void *eacv, Evas_Object *obj, const char *part){
 	struct etac_attr *eac = (void *)eacv;
+	struct neb_attr *subat;
+	int val,val2;
 	char buf[100];
+	const char *sub;
 	Evas_Object *o;
 
 	ETAC_MAGIC_CHECK(eac, NULL);
-//	printf("Part is %s\n",part);
 
-	if (strcmp("elm.swallow.end",part) == 0){
-		o = elm_label_add(obj);
-		snprintf(buf,100,"%d",neb_attr_value_get(eac->attr));
-		elm_label_label_set(o, buf);
-		eac->value = o;
-		return o;
+	if (strcmp("elm.swallow.end",part) != 0)
+		return NULL;
+
+	sub = neb_attr_prop_get(eac,"Subvalue");
+	if (sub){
+		subat = neb_character_attr_get(eac->etac->ch, sub);
+		if (!subat){
+			printf("Unable to find sub attribute\n");
+			return NULL;
+		}
 	}
-	return NULL;
+
+	o = elm_label_add(obj);
+	val = neb_attr_value_get(eac->attr);
+	if (sub) {
+		val2 = neb_attr_value_get(subat);
+		snprintf(buf,100,"%d (%+d)",val,val2);
+	} else
+		snprintf(buf,100,"%d",val);
+	elm_label_label_set(o, buf);
+	eac->value = o;
+	return o;
 }
 static Eina_Bool
 attr_state_get(const void *eacv, Evas_Object *obj, const char *part){
@@ -98,6 +142,27 @@ attr_del(const void *eacv, Evas_Object *obj){
 	return;
 }
 
+/*
+ * Group callbacks
+ */
+static char *
+group_label_get(const void *grpv, Evas_Object *o, const char *part){
+	const struct etac_attr_group *grp = grpv;
+
+	return strdup(grp->group);
+}
+static Evas_Object *
+group_icon_get(const void *grpv, Evas_Object *o, const char *part){
+	return NULL;
+}
+static Eina_Bool
+group_state_get(const void *grpv, Evas_Object *o, const char *part){
+	return true;
+}
+static void
+group_del(const void *grpv, Evas_Object *o){
+	printf("Not implemented\n");
+}
 
 
 
@@ -107,15 +172,35 @@ etac_attr_append(const void *container, void *attrv, void *etacv){
 	struct neb_attr *attr = attrv;
 	struct etac_attr *eac;
 	struct etac *etac = etacv;
+	struct etac_attr_group *grp;
+	Eina_List *l;
 	const char *name;
+	const char *group;
 
+	group = neb_attr_prop_get(attr, "Group");
 	name = neb_attr_name_get(attr);
+	printf("Group for %s is %s\n",name, group);
+	if (!group) return true;
+
+	EINA_LIST_FOREACH(groups, l, grp){
+		if (strcmp(grp->group, group) == 0)
+			break;
+	}
+	if (l == NULL){
+		grp = malloc(sizeof(struct etac_attr_group));
+		grp->item = elm_genlist_item_append(etac->gl, &group_class, grp,
+				NULL,0,NULL,0);
+		grp->group = strdup(group);
+		groups = eina_list_append(groups, grp);
+	}
+
 	printf("Add attr %s\n",name);
 
 	eac = malloc(sizeof(struct etac_attr));
 	eac->magic = ETAC_ATTR_MAGIC;
 	eac->attr = attr;
-	elm_genlist_item_append(etac->gl, &attr_class, eac, NULL, 0, NULL, 0);
+	elm_genlist_item_append(etac->gl, &attr_class, eac, grp->item,
+			0, NULL, 0);
 
 	return true;
 }
